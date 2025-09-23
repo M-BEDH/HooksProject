9e251c046c3b438c032a1e46e4cead89626a8f24
M-BEDH <mel.b.ms@pm.me>
2025-09-23 15:34:54 +0200

    simplification utilisation useContext -> ContentData

--- hooksproject/src/components/ContentData.jsx
+++ hooksproject/src/components/ContentData.jsx
@@ -1,33 +1,19 @@
+import { useContext } from "react"
 import { UserContext, ColorContext } from "./MyContext"
 
 const ContentData = () => {
 
-    return (
-        <UserContext.Consumer>
-            {
-                user => {
-                    return (
-                        <ColorContext.Consumer>
-                            {
-                                color => {
-                                    return (
-                                    <div style={{ color: color }}>
-                                        <ul>
-                                            <li> Nom : {user.name} </li>
-                                            <li> Age : {user.age} </li>
-                                        </ul>
-                                    </div>
-                                    )
-                                }
-                            }
-                        </ColorContext.Consumer>
-                    )
+    const user = useContext(UserContext)
+    const color = useContext(ColorContext)
 
-                }
-            }
-
-        </UserContext.Consumer>
-    )
+            return (
+                <div style={{ color: color }}>
+                    <ul>
+                        <li> Nom : {user.name} </li>
+                        <li> Age : {user.age} </li>
+                    </ul>
+                </div>
+            )
 }
 
 
