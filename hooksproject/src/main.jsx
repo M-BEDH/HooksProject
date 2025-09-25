b3913f03ead068df5c13d4fdf9e3938418ffcaa3
M-BEDH <mel.b.ms@pm.me>
2025-09-25 10:06:58 +0200

    rename App -> AppOld5

--- hooksproject/src/main.jsx
+++ hooksproject/src/main.jsx
@@ -14,6 +14,8 @@ import App from './App.jsx'
 
 // import App from './AppOld4.jsx'
 
+// import App from './AppOld5.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
