70223a9585fa4d1ba6f06ab690574816971ae81e
M-BEDH <mel.b.ms@pm.me>
2025-09-25 08:24:53 +0200

    edit affichage progressBar

--- hooksproject/src/components/ProgressBar.jsx
+++ hooksproject/src/components/ProgressBar.jsx
@@ -6,7 +6,7 @@ const ProgressBar = React.memo(function ProgressBar({ text, count, bgColor }) {
   return (
     <div>
       <p className="h1">{text} </p>
-      <p>{count} %</p>
+      <p className="text-2xl font-bold mb-2">Pourcentage de progression {count} %</p>
       <div className="progress">
         <div
           className={`progress-bar progress-bar-striped bg-${bgColor}`}
@@ -14,7 +14,6 @@ const ProgressBar = React.memo(function ProgressBar({ text, count, bgColor }) {
           style={progress}
         ></div>
       </div>
-      <p className="text-2xl font-bold mb-2">Pourcentage de progression</p>
     </div>
   );
 });
