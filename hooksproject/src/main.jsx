986002f5f2ffbccb693c8962d86efe71be6e342f
M-BEDH <mel.b.ms@pm.me>
2025-09-25 08:27:31 +0200

    edit main.jsx pour inclure et commenter les OldApp

--- hooksproject/src/main.jsx
+++ hooksproject/src/main.jsx
@@ -1,7 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './AppOld4.jsx'
+
+import App from './App.jsx'
+
+// import App from './AppOld.jsx'
+
+// import App from './AppOld1.jsx'
+
+// import App from './AppOld2.jsx'
+
+// import App from './AppOld3.jsx'
+
+// import App from './AppOld4.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
