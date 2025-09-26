e8819fe0a2b5d51e77e3af633595743ecd9605ac
M-BEDH <mel.b.ms@pm.me>
2025-09-26 11:07:19 +0200

    fetch

--- hooksproject/src/components/MyContacts.jsx
+++ hooksproject/src/components/MyContacts.jsx
@@ -4,7 +4,12 @@ import Search from "./Search"
 
 const MyContacts = (text) => {
 
+    const [users, setUsers] = useState([])
     const [search, setSearch] = useState('')
+
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
     // console.log(search)
 
     useUpdateDocTitle(text)
